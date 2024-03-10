7bb61eca0abbd4803bd62b47b1cf8f201c00afd3
taehwan heo <htth815@gmail.com>
2024-03-11 01:57:27 +0900

    [기초] 8479번 림보게임

---
+++ example/1st_round/LV0_Basic/page2/8479_Answer.py
@@ -0,0 +1,19 @@
+import sys
+
+
+def limbo_check(limit:int, length_data:list) -> str:
+    for length in length_data:
+        if length <= limit:
+            return f'I {length}'
+    return 'P'
+
+if __name__=="__main__":
+    LENGTH_GUNHYUK = 160        
+    read = sys.stdin.readline
+    write = sys.stdout.write
+    
+    N = int(read())
+    length_data = list(map(int, read().split()))
+    
+    result = limbo_check(LENGTH_GUNHYUK, length_data)    
+    write(result)
